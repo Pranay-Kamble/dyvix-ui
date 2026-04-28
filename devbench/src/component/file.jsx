32eda3f69f36cab7a41d026697f5d29794e3a243
younisdev <younisayoub10@gmail.com>
2026-04-29 02:47:24 +0300

    Add forest file theme.

--- devbench/src/component/file.jsx
+++ devbench/src/component/file.jsx
@@ -6,7 +6,7 @@ export function FileTest() {
       <DyvixFile
         onUpload={(data) => console.log(data)}
         multiple={true}
-        theme={DYVIX_MODAL_THEME.OCEAN}
+        theme={DYVIX_MODAL_THEME.FOREST}
         animation={"fade"}
         accept={".jpg, .jpeg, .png"}
       >
